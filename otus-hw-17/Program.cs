edfbd2373ad7b281d35aacff1ee1b1b81ae1e058
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-07-14 20:43:30 +0300

    task 2 and 3

--- otus-hw-17/Program.cs
+++ otus-hw-17/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography.X509Certificates;
@@ -9,10 +10,9 @@ Console.WriteLine("Hello, World!");
 string dirName = "C:\\Users\\o.rovenskiy\\source\\repos\\otus-hw-17\\otus-hw-17\\otus-hw-17\\bin\\Debug\\net7.0\\";
 
 
-
 var test = new FileInDirectory(dirName);
 
-test.FileFoundEvent += DisplayMessage;
+test.FileFound += FileEventHandler;
 
 test.GetFile();
 
@@ -26,12 +26,21 @@ test.GetFile();
 Console.ReadKey();
 
 
-void DisplayMessage(string message) => Console.WriteLine(message);
+//void DisplayMessage(string message) => Console.WriteLine(message);
+
+
+static void FileEventHandler(object sender, FileArgs e)
+{
+    Console.WriteLine($"File found {e.FileName}");
+}
 
 public class FileInDirectory
 {
-    public delegate void FileHandler(string message);
-    public event FileHandler FileFoundEvent;
+    //  public delegate void EventHandler(object sender, EventArgs e);
+
+    public delegate void EventHandler (object sender, FileArgs e);
+
+    public event EventHandler FileFound;
     public string dirPath { get; set; }
 
     public FileInDirectory(string _dirPath)
@@ -47,11 +56,21 @@ public class FileInDirectory
         {
             if (s != null)
             {
-                FileFoundEvent?.Invoke($"Файл найден, имя - : {s}");
+                FileFound?.Invoke(this, new FileArgs(s));
             }
         }
     }
 
+}
+
+public class FileArgs : EventArgs
+{
+    public string FileName {get; set;}
+
+    public FileArgs (string fileName)
+    {
+        FileName = fileName;
+    }   
 }
 
 
