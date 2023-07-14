65d27fcb0c042bfdb1cd2cb0073de99ce0a9486e
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-07-14 10:28:30 +0300

    Task-1 final

--- otus-hw-17/Program.cs
+++ otus-hw-17/Program.cs
@@ -6,18 +6,14 @@ using System.Security.Cryptography.X509Certificates;
 Console.WriteLine("Hello, World!");
 
 
-
+// для тестовой коллекции
 var x1 = new TestClass(2);
 var x2 = new TestClass(15);
 var x3 = new TestClass(9);
 var x4 = new TestClass(3);
 var x5 = new TestClass(29);
 var x6 = new TestClass(4);
-
-
 List<TestClass> TestCollection = new List<TestClass>() { x1, x2, x3, x4, x5, x6};
-
-//var test = TestCollection.GetMax<TestClass>(x => (float)Convert.ToDecimal(x));
 
 var test = TestCollection.GetMax<TestClass>(ToNumber);
 
@@ -30,30 +26,24 @@ Console.ReadKey();
 // преобразующий входной тип в число
 float ToNumber <T> (T t) where T : class
 {
-
     Type myType = typeof(T);
     float num = 0; 
 
     foreach (var l in myType.GetProperties())
     {
-
-        var ff = myType.GetProperty(l.Name);
-        var ff1 = ff?.GetValue(t);
-        num = num + (float) Convert.ToDecimal(ff1);
+        var ff = (myType.GetProperty(l.Name))?.GetValue(t);
+        num = num + (float) Convert.ToDecimal(ff);
     }
     return num;
 
 }
 
-
 // Метод расширения поиска Максимального
 public static class CollectionExtention
 {
-
     public static T GetMax<T>(this IEnumerable e, Func<T, float> getParameter) where T : class
     {
-            
-        T MaxT = null;
+        T? MaxT = default(T);
         float f = float.MinValue;
 
         foreach (T i in e)
@@ -65,7 +55,6 @@ public static class CollectionExtention
                 f = d;
             }
         }
-
         return MaxT;
     }
 }
