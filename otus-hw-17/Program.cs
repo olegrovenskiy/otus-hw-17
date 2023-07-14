e32f4b5f98c3d4f0fd3f0c01dd214a4f6a4346af
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-07-14 19:15:20 +0300

    Task-2

--- otus-hw-17/Program.cs
+++ otus-hw-17/Program.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 
 Console.WriteLine("Hello, World!");
+
+string dirName = "C:\\Users\\o.rovenskiy\\source\\repos\\otus-hw-17\\otus-hw-17\\otus-hw-17\\bin\\Debug\\net7.0\\";
+
+
+
+var test = new FileInDirectory(dirName);
+
+test.FileFoundEvent += DisplayMessage;
+
+test.GetFile();
+
 
 
 
@@ -12,4 +24,37 @@ Console.WriteLine("Hello, World!");
 
 
 Console.ReadKey();
+
+
+void DisplayMessage(string message) => Console.WriteLine(message);
+
+public class FileInDirectory
+{
+    public delegate void FileHandler(string message);
+    public event FileHandler FileFoundEvent;
+    public string dirPath { get; set; }
+
+    public FileInDirectory(string _dirPath)
+    {
+        dirPath = _dirPath;
+    }
+
+    public void GetFile ()
+    {
+        string[] files = Directory.GetFiles(dirPath);
+
+        foreach (string s in files)
+        {
+            if (s != null)
+            {
+                FileFoundEvent?.Invoke($"Файл найден, имя - : {s}");
+            }
+        }
+    }
+
+}
+
+
+
+
 
