5d0fff4815cfa66625e2a8b747c35a0bde3cca9f
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-07-15 08:57:47 +0300

    task-4

--- otus-hw-17/Program.cs
+++ otus-hw-17/Program.cs
@@ -9,11 +9,11 @@ Console.WriteLine("Hello, World!");
 
 string dirName = "C:\\Users\\o.rovenskiy\\source\\repos\\otus-hw-17\\otus-hw-17\\otus-hw-17\\bin\\Debug\\net7.0\\";
 
-int FoundResult = 0;
+//public int FoundResult = 0;
 
 var test = new FileInDirectory(dirName);
 
-test.FileFound += FileEventHandler;
+//test.FileFound += FileEventHandler;
 
 
 
@@ -31,29 +31,55 @@ Console.ReadKey();
 
 //void DisplayMessage(string message) => Console.WriteLine(message);
 
-
+/*
 static void FileEventHandler(object sender, FileArgs e)
 {
+   
     Console.WriteLine($"File found {e.FileName}");
     if (e.FileName.Contains("exe"))
     {
         Console.WriteLine("One file *.exe");
-
+        
     }
 
 }
+
+*/
+
 
 public class FileInDirectory
 {
     public delegate void EventHandler (object sender, FileArgs e);
 
     public event EventHandler FileFound;
+
+    public int FoundResult = 0;
+
     public string dirPath { get; set; }
     public int Check = 0;
     public FileInDirectory(string _dirPath)
     {
         dirPath = _dirPath;
+        this.FileFound += FileEventHandler;
     }
+
+
+    public void FileEventHandler(object sender, FileArgs e)
+    {
+
+        Console.WriteLine($"File found {e.FileName}");
+        if (e.FileName.Contains("exe"))
+        {
+            Console.WriteLine("One file *.exe");
+            FoundResult = 1;
+        }
+
+    }
+
+
+
+
+
 
     public void GetFile ()
     {
@@ -64,7 +90,10 @@ public class FileInDirectory
             if (s != null)
             {
                 FileFound?.Invoke(this, new FileArgs(s));
+                
             }
+            if (FoundResult == 1)
+                break;
         }
     }
 
